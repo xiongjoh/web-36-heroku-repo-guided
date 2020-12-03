e8c26e3178daf381e7f0d2b4d14fed46e642d2cb
unknown <xiongjoh@gmail.com>
2020-12-03 12:34:28 -0600

    wip

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 4000
+const path = require('path') // utility for adding functionality to paths
 
 console.log('web 36 rocks')
 console.log(__dirname)
@@ -18,12 +19,18 @@ const app = express()
 // middleware
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build'))) // teach where the static assets live
 
 // api, routers
 
-// catch all
+// anything in here is our api
 app.use('/api/*', (_, res) => {
     res.json({json:'web 36 rocks'})
+})
+
+// anything in here is our react-page
+app.use('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.listen(port, () => {
