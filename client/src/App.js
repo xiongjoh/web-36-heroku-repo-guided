c993fdc5a4129b3af36e5db4779048cc031f18fe
unknown <xiongjoh@gmail.com>
2020-12-03 12:53:42 -0600

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
+
+// development
+// production
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+  ? `http://localhost:5000${[path]}`
+  : ``
+
+}
+
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    // heroku.com/nice-app
+    // localhost:5000
+    fetch(url('/api/data'))
+    .then(res => res.json())
+    .then(goods => setData(goods.json))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        WEB 36 ROCKSSFSSFSFS
+        {data}
       </header>
     </div>
   );
